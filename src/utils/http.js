b8937842e51b7d810fce798c007ad602ff826c9b
zhaizhaohui <311551997@qq.com>
2023-12-28 10:35:30 +0800

    Added axios encapsulation

---
+++ src/utils/http.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const http = axios.create({
+	baseURL: '',
+	timeout: 600
+})
+
+http.interceptors.request.use((config)=> {
+	return config
+}, (error) => {
+	return Promise.reject(error)
+})
+
+
+http.interceptors.response.use((response)=> {
+	return response
+}, (error) => {
+	return Promise.reject(error)
+})
+
+export { http }
